241b0b7d643233a134f1531c589520c2ad10e3e5
hinto <hinto@rikt.ru>
2025-10-05 04:10:16 +0700

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,56 @@
 #include "timer.h"
 #include "serif_18i.h"
 
-/*
+
 LCD lcd;
 int mode = 1;
 
 int main(void)
 {
-  SPI0_SCK.init(GPO_Max);
+  T1_DIV_1024;
+  TCNT1 = 0;
 
   lcd.init();
   lcd.font(serif_18i, 0, 0);
   lcd.color(White);
   lcd.background(MidnightBlue);
+  // lcd.clear();
 
   // timer.enable();
 
   int x = 0;
   while (true) {
-
-    SPI0_SCK.inv();
-    delay_ms(1000);
+    TCNT1 = 0;
+    lcd.demo(x++);
     // if (USER_B.get()) {
     //   while (USER_B.get());
     //   mode++;
     // }
     // timer.clear();
 
-    switch (mode) {
-      case 0:
-        lcd.background(color[x++ & 0x7F]);
-        lcd.clear();
-        break;
-      case 1: lcd.demo(x++); break;
-        // case 2: lcd.demo2(x++); break;
-      case 2: lcd.demo3(x++); break;
+    // switch (mode) {
+    //   case 0:
+    //     lcd.background(color[x++ & 0x7F]);
+    //     lcd.clear();
+    //     break;
+    //   case 1: lcd.demo(x++); break;
+    //     // case 2: lcd.demo2(x++); break;
+    //   case 2: lcd.demo3(x++); break;
 
-      default: mode = 0;
-    }
+    //   default: mode = 0;
+    // }
 
-    // uint32_t fps = (F_CPU << 4) / timer.CNT();
+    u16 fps = ((F_CPU >> 6) / TCNT1);
     lcd.at(0, lcd.max_y() - 2 * lcd.get_height() + 1);
     lcd.color(White);
     lcd.background(MidnightBlue);
-    //   lcd.printf(
-    //     "FPS: %.2.4q\n%u X %u X %u", fps,
-    //     lcd.max_x() + 1, lcd.max_y() + 1, RGB::len());
+    lcd.printf(
+      P("FPS: %.2.4q\n%u X %u X %u"), fps,
+      lcd.max_x() + 1, lcd.max_y() + 1, RGB::len());
   }
 }
-*/
 
+/*
 int main(void)
 {
   SPI0_SCK.init(GPO_Max);
@@ -59,6 +60,8 @@ int main(void)
   while (true) {
 
     SPI0_SCK.inv();
-    delay_ms(1000);
+    delay_ms(100);
   }
 }
+
+*/
