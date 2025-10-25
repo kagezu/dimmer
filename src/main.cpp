702d849eea2358389f49fd4e4231030233e94d27
hinto <hinto@rikt.ru>
2025-10-25 20:34:58 +0700

    реализовано управление мощностью

--- src/main.cpp
+++ src/main.cpp
@@ -9,46 +9,41 @@
 #include "encoder.h"
 
 #define max(a,b) ((a)>(b)?(a):(b))
-#define AVG_FACTOR 5
 
 ADC adc;
 LCD lcd;
 
 Pin<PB, 1> PWM;
-Pin<PC, 3> CAP_RST;
+Pin<PC, 3> CAP;
 Encoder enc;
 
-
-Average<uint16_t, AVG_FACTOR> a1;
-Average<uint32_t, AVG_FACTOR> a2;
-
-volatile uint8_t f = 0;
-
-volatile uint16_t t1;
-volatile uint16_t t2;
-
+volatile uint8_t f;
 volatile uint16_t v1;
 volatile uint16_t v2;
 
-#define CUR_A     2.714f
-#define CUR_R     7.384f
-#define CUR_AR    2.7268f
-#define CUR_A_MIN 30
-#define CUR_R_MIN 11
+Average<int32_t, 7> e_avg;
+
+///////////////////////////////////////////////////////////////////////////////
+
+volatile int16_t energy_sum = 0;
+volatile int16_t energy_target = 0;
+volatile int16_t energy_last = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+
+#define CUR_K1    7.384f
+#define CUR_K2    8.1804f
+#define CUR_MIN   11
 
 volatile uint8_t reset = 0;
 
 uint16_t current(uint16_t arg)
 {
+  uint16_t result;
   if (arg == 0) return 0;
-  if (reset) {
-    if (arg > CUR_R_MIN) return 140 + (arg - CUR_R_MIN) * CUR_R;
-    return 50 + arg * 3 * CUR_AR;
-  }
-  else {
-    if (arg > CUR_A_MIN) return 140 + (arg - CUR_A_MIN) * CUR_A;
-    return 47 + arg * 3;
-  }
+  if (arg > CUR_MIN) result = 59 + arg * CUR_K1;
+  else result = 50 + arg * CUR_K2;
+  return result;
 }
 
 int main(void)
@@ -59,17 +54,12 @@ int main(void)
   T0_DIV_64;
   T0_OVF_ON;
 
-  T1_DIV_8;
-  T1_FAST_PWM_CUSTOM;
-  T1_OC1A_PWM_INV;
-  T1_COMPA_ON;
-  ICR1 = -1;
-  OCR1A = 20000;
   PWM.init(GPO_Max);
-  enc.count = 199;
+  PWM.clr();
+  CAP.init(GP_Float);
+  CAP.clr();
 
-  CAP_RST.init(GP_Float);
-  CAP_RST.clr();
+  enc.count = 0;
 
   adc.init(6, ADC_DIV_16);
   adc.start();
@@ -80,51 +70,43 @@ int main(void)
   lcd.clear();
 
   sei();
-  delay_ms(30);
-  a1 = t1;
-  a2 = t2;
 
   while (true) {
-    lcd.printf(P("\fT1: %u      \n"), a1.value >> 6);
-    lcd.printf(P("T2: %u      \n"), (u32)(a2.value >> 6));
-    lcd.printf(P("Value: %u      \n"), max(v1, v2));
-    lcd.printf(P("Current: %u      \n"), current(max(v1, v2)));
+    lcd.printf(P("\fCurrent: %u      \n"), max(v1, v2));
+    lcd.printf(P("Power target: %u      \n"), enc.count >> 2);
+    lcd.printf(P("Energy: %u      \n"), (uint16_t)e_avg / 10);
     lcd.printf(P("\nEncoder: %c %u       \n"),
-      enc.is_push() ? '+' : '-', enc.count);
+      enc.is_push() ? '+' : '-', enc.count >> 2);
   }
 }
 
 ISR(INT0_vect)
 {
-  t2 = TCNT1;
-  TCNT1 = -1;
-  a2 += t2;
+  uint16_t value = current(adc.value());
+  CAP.init(GPO_Max); // Сброс конденсатора
+  if (f) { v1 = value; f = 0; }
+  else { v2 = value; f = 1; }
 
-  if (f) { v1 = adc.value(); f = 0; }
-  else { v2 = adc.value(); f = 1; }
+  energy_last = (value << 1) + (value >> 2); // I * 225V / 100 = I * 2.25 mJ
+  energy_last >>= 3;
+  energy_sum += energy_last - energy_target; // Перерасход энергии mJ / полупериод
 
-  if (reset) CAP_RST.init(GPO_Max); // Сброс конденсатора
-  OCR1A = enc.count * 100;
+  if (energy_sum < 0) energy_sum = 0;
+  if (energy_sum < energy_target) PWM.set(); // Открыть симистор
+
+  e_avg += energy_last;
 }
 
 ISR(INT1_vect)
 {
-  t1 = TCNT1;
-  TCNT1 = t1 >> 1;
-  a1 += t1;
-
-}
-
-ISR(TIMER1_COMPA_vect)
-{
-  delay_us(10);
-  CAP_RST.init(GP_Float);
+  CAP.init(GP_Float); // Начать накопление
+  PWM.clr(); // Отключить открывающий ток
 }
 
 ISR(TIMER0_OVF_vect)
 {
-  sei();
   enc.scan();
-  if (enc.is_push()) reset = 1;
-  else reset = 0;
+  if (enc.count < 0) enc.count = 0;
+  if (enc.count > 3999) enc.count = 3999;
+  energy_target = (enc.count * 10) >> 2; // mJ на полупериод
 }
