5d92f710aa06db013502110b8d1168367a7b1063
hinto <hinto@rikt.ru>
2025-10-26 18:41:40 +0700

    alpha релиз (не откалибровано)

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 #define max(a,b) ((a)>(b)?(a):(b))
 #define E_HISTORY 100
-#define MAX_CURRENT 6000
+#define MAX_CURRENT 5000
 
 ADC adc;
 SPI spi;
@@ -19,6 +19,21 @@ Port<PC, 0b111> DIGIT;
 Pin<PD, 4> USER;
 
 Encoder enc;
+
+///////////////////////////////////////////////////////////////////////////////
+
+volatile int16_t energy_mod = 0;
+volatile int32_t energy_sum = 0;
+
+volatile int16_t energy_last[E_HISTORY] = {};
+volatile uint8_t index = 0;
+
+volatile int8_t push = 0;
+volatile int8_t block = 0;
+volatile int16_t digit = 1;
+volatile int16_t power = 0;
+
+volatile int16_t delay = 0;
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -35,6 +50,7 @@ uint8_t number[] = {
   0b0010000,  // 9
   0b0001000,  // A
   0b1111111,  // 
+  0b0111111,  // -
 };
 
 volatile uint8_t out[3] = { 11,11,11 };
@@ -44,45 +60,31 @@ void print(uint16_t n)
 {
   uint8_t d2 = n / 100;
   uint8_t d1 = n / 10 - d2 * 10;
-  out[0] = n % 10;
+  out[0] = number[n % 10];
   if (d2 == 0) {
-    out[2] = 11;
-    if (d1 == 0)out[1] = 11; else out[1] = d1;
+    out[2] = number[11];
+    if (d1 == 0)out[1] = number[11]; else out[1] = number[d1];
   }
-  else { out[2] = d2; out[1] = d1; }
-  // if (!n) out[0] = 11;
+  else { out[2] = number[d2]; out[1] = number[d1]; }
+
+  if (USER.get()) out[2] = number[12];
 }
-
-///////////////////////////////////////////////////////////////////////////////
-
-volatile int16_t energy_mod = 0;
-volatile int16_t energy_target = 0;
-volatile int32_t energy_sum = 0;
-volatile uint16_t energy = 0;
-
-int16_t energy_last[E_HISTORY] = {};
-int8_t index = 0;
-
-volatile int8_t push = 0;
-volatile int16_t digit = 1;
-volatile int16_t power = 0;
-
-volatile int16_t delay = 0;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 #define CUR_K1    7.384f
 #define CUR_K2    8.1804f
+#define CUR_K3    10.93f
 #define CUR_MIN   11
-
-volatile uint8_t reset = 0;
 
 uint16_t current(uint16_t arg)
 {
   uint16_t result;
   if (arg == 0) return 0;
-  if (arg > CUR_MIN) result = 59 + arg * CUR_K1;
-  else result = 50 + arg * CUR_K2;
+  // if (arg > CUR_MIN) result = 59 + arg * CUR_K1;
+  // else result = 50 + arg * CUR_K2;
+  if (arg < 200) result = 60 + arg * 9.39f;
+  else result = arg * CUR_K3 - 188;
   return result;
 }
 
@@ -90,12 +92,6 @@ uint16_t current(uint16_t arg)
 
 int main(void)
 {
-  EICRA = 0b1011; // INT1 INT0
-  EIMSK = 0b11;
-
-  T0_DIV_64;
-  T0_OVF_ON;
-
   PWM.init(GPO_Max);
   PWM.clr();
   CAP.init(GP_Float);
@@ -103,6 +99,12 @@ int main(void)
   USER.init(GP_Float);
   DIGIT.init(GPO_Max);
   DIGIT.set(0b111);
+
+  EICRA = 0b1011; // INT1 INT0
+  EIMSK = 0b11;
+
+  T0_DIV_64;
+  T0_OVF_ON;
 
   delay_ms(100);
   adc.init(6, ADC_DIV_16);
@@ -113,23 +115,32 @@ int main(void)
   sei();
 
   while (true) {
-    // lcd.printf(P("Power: %u      \n"), power);
-    // lcd.printf(P("Energy x10: %u      \n"), energy_sum / E_HISTORY);
-    if (delay)print(power);
-    else print(energy_sum / E_HISTORY / 10);
+    if (block) break;
+    if (delay) print(power);
+    else print(energy_sum / (E_HISTORY * 10));
     char i = 100;
     while (i-- && !delay) delay_ms(5);
   }
+
+  out[0] = out[1] = out[2] = 63;
+  while (true);
 }
 
 ISR(INT0_vect)
 {
   uint16_t value = current(adc.value());
-  // if (value > MAX_CURRENT) { power = 0; energy_target = 0; } // Ограничитель тока
+  if (value > MAX_CURRENT) { // Блокировка
+    block = 1;
+    out[2] = 63;
+    out[1] = 63;
+    out[0] = 63;
+  }
   CAP.init(GPO_Max);                          // Сброс конденсатора
+  if (block) return;
 
-  energy = (value << 1) + (value >> 2);  // I * 225V / 100 = I * 2.25 mJ
-  energy_mod += energy - energy_target;       // Перерасход энергии mJ / полупериод
+  int16_t energy = (value << 1) + (value >> 2); // I * 225V / 100 = I * 2.25 mJ
+  int16_t energy_target = power * 10;           // mJ на полупериод
+  energy_mod += energy - energy_target;         // Перерасход энергии mJ / полупериод
 
   if (energy_mod < 0) energy_mod = 0;
   if (energy_mod < energy_target) PWM.set();  // Открыть симистор
@@ -147,7 +158,7 @@ ISR(INT1_vect)
 
 ISR(TIMER0_OVF_vect)
 {
-  // if (USER.get()) power = 0;
+  if (USER.get()) asm volatile("jmp 0");
 
   if (enc.is_push()) push = 1;
   else if (push) { push = 0; digit *= 10; }
@@ -157,11 +168,10 @@ ISR(TIMER0_OVF_vect)
   power += inc * digit;
   if (power < 0) power = 0;
   if (power > 1000) power = 1000;
-  energy_target = power * 10; // mJ на полупериод
 
   if (out_i++ == 2) out_i = 0;
   DIGIT.set(0b111);
-  spi.send(number[out[out_i]]);
+  spi.send(out[out_i]);
   spi.wait();
   DIGIT.clr(1 << out_i);
 
